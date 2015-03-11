62823567205f3e503688cd4026e258d0dd786256
Diego Vera <diego.vera@prodigious.cr>
2015-03-10 20:56:25 -0600

    clase semana 8

--- server.js
+++ server.js
@@ -1,31 +1,45 @@
 var express = require('express');
 
 var app = express();
+var appFortune = require("./lib/fortune");
 
+// set up handlebars view engine
 var handlebars = require('express3-handlebars')
-          .create({ defaultLayout:'main'});
-
+	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');          
+app.set('view engine', 'handlebars');
 
+app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 3000);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	res.render('home');
 });
-
-app.get('/home', function(req, res){
-	res.render('home');
+app.get('/home', function(req, res) {
+	
+	
+	res.render('home', {fortune: appFortune.getFortune()});
 });
-
-app.get('/about', function(req, res){
+app.get('/about', function(req,res){
 	res.render('about');
 });
-app.get('/developers', function(req, res){
-	res.render('developers');
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
 });
 
-console.log('Server started on localhost');
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
