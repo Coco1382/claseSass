d3007f02b2c65e5fdcdc82e8e8c986e743bf497c
Alfredo Arrieta <aac7780@gmail.com>
2015-04-17 19:47:51 -0600

    Add news posts in columns covert inventory to posts

--- credentials.js
+++ credentials.js
@@ -1,5 +1,4 @@
 module.exports = {
 	cookieSecret: 'Ucr3@t1v@',
 	tokenSecret: 'lsM0qCJn3YKGw893acn3Wa7TsStKioORelB1cv2h'
-	//tokenSecret: '5LlDuXKg1MBBTi8ymBCXagduwrKML8oU8f0HoseM'
 };
---
+++ public/js/controllers/postsController.js
@@ -0,0 +1,19 @@
+APP.controller('postsController', function($scope, $firebaseArray) {
+
+	//$scope.universidad = 'creativa';
+
+	$scope.$watchGroup(['user', 'secret'], function(){
+
+		var ref = new Firebase("https://ucreativa1.firebaseio.com/"+$scope.user);
+
+		ref.authWithCustomToken($scope.secret, function(error, authData) {
+			if (error) {
+				console.log("Login Failed!", error);
+			} else {
+				$scope.posts = $firebaseArray(ref);
+			}
+		});
+
+	});
+
+});
