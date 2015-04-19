a288768ca58018266c12f29b387c8729e8bc9193
Alfredo Arrieta <aac7780@gmail.com>
2015-04-18 19:39:04 -0600

    create, update and remove post

--- public/js/controllers/postsController.js
+++ public/js/controllers/postsController.js
@@ -4,16 +4,73 @@ APP.controller('postsController', function($scope, $firebaseArray) {
 
 	$scope.$watchGroup(['user', 'secret'], function(){
 
-		var ref = new Firebase("https://ucreativa1.firebaseio.com/"+$scope.user);
+		var url = "https://ucreativa1.firebaseio.com/" + $scope.user,
+				ref = new Firebase(url);
+				//obj = $firebaseObject(ref);
+
+		//console.log(obj);
 
 		ref.authWithCustomToken($scope.secret, function(error, authData) {
 			if (error) {
 				console.log("Login Failed!", error);
 			} else {
-				$scope.posts = $firebaseArray(ref);
+				var list = $firebaseArray(ref);
+				$scope.posts = list;
+
+				$scope.removeItem = function (index) {
+					//console.log(ref);
+					// Funciona pero hay que hacer un objeto nuevo de Firebase
+					//var itemRef = new Firebase(url + '/' + id);
+					//itemRef.remove();
+
+					//obj.$remove(id); <-- remueve todos los datos UPSSS
+					var item = list[index];
+					list.$remove(item).then(function(ref) {
+						ref.key() === item.$id; // true
+					});
+				};
+
+				$scope.createItem = function () {
+					if ($scope.postTitle && $scope.postDescription && $scope.postImage ) {
+						ref.push({'img': $scope.postImage, 'newsBody': $scope.postDescription, 'title': $scope.postTitle});
+						$scope.postImage = '';
+						$scope.postDescription = '';
+						$scope.postTitle = '';
+					}
+				};
+
+				$scope.editItem = function (index) {
+					var item = list[index];
+					console.log(item);
+					$scope.postIndex = index;
+					$scope.postTitle2 = item.title;
+					$scope.postDescription2 = item.newsBody;
+					$scope.postImage2 = item.img;
+					$scope.showEditForm = true;
+				}
+
+				$scope.saveItem = function (index) {
+					list[index].title = $scope.postTitle2;
+					list[index].newsBody = $scope.postDescription2;
+					list[index].img = $scope.postImage2;
+					list.$save(index).then(function(ref) {
+						ref.key() === list[index].$id; // true
+					});
+					$scope.postTitle2 = '';
+					$scope.postDescription2 = '';
+					$scope.postImage2 = '';
+					$scope.showEditForm = false;
+
+					//$scope.postTitle2 = "funcionando el save";
+
+
+				}
+
+
+
+
 			}
 		});
 
 	});
-
 });
