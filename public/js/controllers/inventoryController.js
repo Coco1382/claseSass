50fd9ab22ca38e24dc2a3c241c43391b6b55293b
eam209 <e_am209@hotmail.com>
2015-04-25 00:42:01 -0600

    Final de trabajo

--- public/js/controllers/inventoryController.js
+++ public/js/controllers/inventoryController.js
@@ -2,21 +2,35 @@ APP.controller('inventoryController', function($scope, $firebaseArray) {
 
 	$scope.$watchGroup(['user', 'secret'], function(){
 
-    	var ref = new Firebase("https://ucreativaeduardo.firebaseio.com/"+$scope.user);
+    	var ref = new Firebase('https://ucreativaeduardo.firebaseio.com/'+$scope.user);
 
         ref.authWithCustomToken($scope.secret, function(error, authData) {
 			if (error) {
 		    	console.log("Login Failed!", error);
 		  	} else {
 		    	$scope.inventory = $firebaseArray(ref);
-
-		    	console.log($scope.inventory);
 			}
 		});
 
 		$scope.remove = function(id){
-			console.log(id);
+			var ref = new Firebase('https://ucreativaeduardo.firebaseio.com/'+$scope.user+'/'+id);
+			ref.remove();
 		};
 
+		$scope.add = function(){
+			var id = document.formulario.id.value;
+			var brand = document.formulario.brand.value;
+			var item = document.formulario.item.value;
+			var quantity = document.formulario.quantity.value;
+
+			var ref = new Firebase("https://ucreativaeduardo.firebaseio.com/"+$scope.user+'/'+id);
+
+			var proc = ref.set({ brand: brand, item: item, quantity: quantity });
+
+			$scope.id = '';
+			$scope.item = '';
+			$scope.brand = '';
+			$scope.quantity = '';
+		}
    	});
 });
